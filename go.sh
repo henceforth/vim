c23bdb525c0343a1319b11a645ec15d01fa6cb14
hcf <electriclarryland@gmail.com>
2013-06-30 21:12:38 +0200

    added up/down switch to script

--- go.sh
+++ go.sh
@@ -1,6 +1,31 @@
 #! /usr/bin/env bash
 
-#mkdir vim
-#cp ~/.vim/* vim -rf
-rsync -ur --progress --exclude=.git* --stats ~/.vim/ vim
-cp ~/.vimrc vimrc
+up(){
+  #pushes to rep
+  echo "pushing local files to repository"
+  rsync -ur --progress --exclude=.git* --stats ~/.vim/ vim
+  cp ~/.vimrc vimrc
+  git add .
+  git commit
+  git push origin master
+}
+
+down(){
+  echo "cloning repository to local"
+  git pull origin master
+  cp vim/* ~/.vim
+  cp vimrc ~/.vimrc
+}
+
+case $1 in
+  "u" | "up")
+    up
+    ;;
+
+  "d" | "down")
+    down
+    ;;
+
+  *)
+    echo "need parameter (u)p or (d)own"
+esac
